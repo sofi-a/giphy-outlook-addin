19e67800b3140122cf94f68ee4e4b547a7660741
Sofonias Abathun <sofoniasabathun@gmail.com>
2023-03-05 15:05:21 +0300

    feat: insert random gif (ui-less button)

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -5,27 +5,74 @@
 
 /* global global, Office, self, window */
 
+const { GIPHY_API_KEY } = process.env;
+
+let isOfficeInitialized = false;
+
 Office.onReady(() => {
-  // If needed, Office.js is ready to be called
+  isOfficeInitialized = true;
 });
 
 /**
+ * Fetchs a random GIF and inserts into the message body
  * Shows a notification when the add-in command is executed.
  * @param event {Office.AddinCommands.Event}
  */
-function action(event) {
-  const message = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
+function insertRandomGIF(event) {
+  if (!isOfficeInitialized) return;
 
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+  let message;
 
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
+  fetch(`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&tag=&rating=g`)
+    .then((response) => response.json())
+    .then(({ data, meta }) => {
+      if (meta.status !== 200) {
+        message = {
+          type: Office.MailboxEnums.ItemNotificationMessageType.ErrorMessage,
+          message: "An error occurred while inserting the GIF. Please try again later.",
+          icon: "Icon.80x80",
+          persistent: true,
+        };
+      } else {
+        Office.context.mailbox.item.body.setSelectedDataAsync(
+          `<div><img src="${data.images.original.url}" /></div>`,
+          { coercionType: Office.CoercionType.Html },
+          (result) => {
+            if (result.status === Office.AsyncResultStatus.Failed) {
+              console.error(result.error.message);
+              message = {
+                type: Office.MailboxEnums.ItemNotificationMessageType.ErrorMessage,
+                message: result.error.message,
+                icon: "Icon.80x80",
+                persistent: true,
+              };
+            } else {
+              message = {
+                type: Office.MailboxEnums.ItemNotificationMessageType.InsightMessage,
+                message: "GIF inserted successfully.",
+                icon: "Icon.80x80",
+                persistent: true,
+              };
+            }
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      message = {
+        type: Office.MailboxEnums.ItemNotificationMessageType.ErrorMessage,
+        message: err.message || "An error occurred while inserting the GIF. Please try again later.",
+        icon: "Icon.80x80",
+        persistent: true,
+      };
+    })
+    .finally(() => {
+      // Show a notification message
+      Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+
+      event.completed();
+    });
 }
 
 function getGlobal() {
@@ -41,4 +88,4 @@ function getGlobal() {
 const g = getGlobal();
 
 // The add-in command functions need to be available in global scope
-g.action = action;
+g.insertRandomGIF = insertRandomGIF;
