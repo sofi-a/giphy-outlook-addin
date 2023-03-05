285a4d6b53126ac9a7eeec6e14bb176a0876b3fb
Sofonias Abathun <sofoniasabathun@gmail.com>
2023-03-05 12:12:18 +0300

    feat: api call & pagination

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -1,12 +1,60 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Image, ImageFit, Spinner, SpinnerSize, Stack } from "@fluentui/react";
 import GIFs from "./GIFs";
-import gifs from "./gifs.json";
+import Pagination from "./Pagination";
 
-/* global require */
+const { GIPHY_API_KEY } = process.env;
 
 export default function App({ isOfficeInitialized }) {
+  const [gifs, setGifs] = useState([]);
+  const [pagination, setPagination] = useState({
+    total_count: 0,
+    count: 25,
+    offset: 0,
+  });
+  const [loading, setLoading] = useState(false);
+
+  const fetchGifs = async ({ count, offset, q, endpoint = "trending" }) => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/${endpoint}?api_key=${GIPHY_API_KEY}&limit=${count}&offset=${offset}${
+          endpoint === "search" ? `&q=${q}` : ""
+        }`
+      );
+      const { data, pagination } = await response.json();
+      setGifs(data);
+      setPagination(pagination);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const next = () => {
+    setPagination((prev) => {
+      fetchGifs({ count: prev.count, offset: prev.offset + prev.count });
+      return { ...prev, offset: prev.offset + prev.count };
+    });
+    window.scrollTo(0, 0);
+  };
+
+  const prev = () => {
+    setPagination((prev) => {
+      fetchGifs({ count: prev.count, offset: prev.offset - prev.count });
+      return { ...prev, offset: prev.offset - prev.count };
+    });
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    if (isOfficeInitialized) {
+      fetchGifs({ count: pagination.count, offset: pagination.offset });
+    }
+  }, [isOfficeInitialized]);
+
   if (!isOfficeInitialized) {
     return (
       <Stack verticalAlign="center" style={{ height: "100vh" }}>
@@ -17,7 +65,8 @@ export default function App({ isOfficeInitialized }) {
 
   return (
     <Stack tokens={{ childrenGap: 20 }}>
-      <GIFs gifs={gifs} />
+      <GIFs gifs={gifs} loading={loading} />
+      <Pagination pagination={pagination} next={next} prev={prev} />
       <Image imageFit={ImageFit.contain} src={require("./../../../assets/attribution-mark.png")} />
     </Stack>
   );
