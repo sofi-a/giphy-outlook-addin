29daa1e5a977393ee0932651ee5748c306c8a83e
Sofonias Abathun <sofoniasabathun@gmail.com>
2023-03-05 10:40:28 +0300

    feat: gif listing & giphy attribution mark

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -1,75 +1,28 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { DefaultButton } from "@fluentui/react";
-import Header from "./Header";
-import HeroList from "./HeroList";
-import Progress from "./Progress";
+import { Image, ImageFit, Spinner, SpinnerSize, Stack } from "@fluentui/react";
+import GIFs from "./GIFs";
+import gifs from "./gifs.json";
 
 /* global require */
 
-export default class App extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      listItems: [],
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      listItems: [
-        {
-          icon: "Ribbon",
-          primaryText: "Achieve more with Office integration",
-        },
-        {
-          icon: "Unlock",
-          primaryText: "Unlock features and functionality",
-        },
-        {
-          icon: "Design",
-          primaryText: "Create and visualize like a pro",
-        },
-      ],
-    });
-  }
-
-  click = async () => {
-    /**
-     * Insert your Outlook code here
-     */
-  };
-
-  render() {
-    const { title, isOfficeInitialized } = this.props;
-
-    if (!isOfficeInitialized) {
-      return (
-        <Progress
-          title={title}
-          logo={require("./../../../assets/logo-filled.png")}
-          message="Please sideload your addin to see app body."
-        />
-      );
-    }
-
+export default function App({ isOfficeInitialized }) {
+  if (!isOfficeInitialized) {
     return (
-      <div className="ms-welcome">
-        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
-        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
-            Modify the source files, then click <b>Run</b>.
-          </p>
-          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
-            Run
-          </DefaultButton>
-        </HeroList>
-      </div>
+      <Stack verticalAlign="center" style={{ height: "100vh" }}>
+        <Spinner size={SpinnerSize.large} label="Initializing Add-in" />;
+      </Stack>
     );
   }
+
+  return (
+    <Stack tokens={{ childrenGap: 20 }}>
+      <GIFs gifs={gifs} />
+      <Image imageFit={ImageFit.contain} src={require("./../../../assets/attribution-mark.png")} />
+    </Stack>
+  );
 }
 
 App.propTypes = {
-  title: PropTypes.string,
   isOfficeInitialized: PropTypes.bool,
 };
